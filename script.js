f33ec162cfa7ae02c49b40234341a2ef9a8ebf2f
JaimeLHF <102550891+JaimeLHF@users.noreply.github.com>
2023-10-19 09:50:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,90 +1,81 @@
 
-setInterval(function() {
-    let city = document.getElementById('city')
-    const today = new Date(). toLocaleTimeString();
-    city.textContent = "São José do Cedro " +today
+setInterval(function () {
+
+
+
 }, 1000);
 
-window.addEventListener('load', ()=> { 
+window.addEventListener('load', () => {
 
-    var config = {
-        apiKey: "AIzaSyBbKFyTLgdb6BZUasgj8chUtib1Y3YRcJ4",
-        authDomain: "temp-1ac8c.firebaseapp.com",
-        databaseURL: "https://temp-1ac8c-default-rtdb.firebaseio.com",
-        projectId: "temp-1ac8c",
-        storageBucket: "temp-1ac8c.appspot.com",
-        messagingSenderId: "5903967480",
-        appId: "1:5903967480:web:e3cba1ea0ea0526c88c70d",
-        measurementId: "G-2TN1XJREPE"
-        };
-        
-        firebase.initializeApp(config);
-        
-        let temp = document.getElementById('temp')
-        setInterval(function(){
-        var db = firebaseRef = firebase.database().ref().child("Hasil_Pembacaan")
-        db.on('child_added', function(snapshot){
-            var valor = snapshot.val();
-            temp.innerHTML = valor + " °C"
-        })}, 1000);
-           
 
-    let lon
-    let lat
-        
+    let city = document.getElementById('city')
+    let temp = document.getElementById('temp')
     let descricao = document.getElementById('descr')
     let latitude = document.getElementById('lat')
     let longitude = document.getElementById('long')
     let vento = document.getElementById('vento')
     let icon = document.getElementById('icone')
 
-        
-      
-                      
-            lon = -81.250853
-            lat = 42.988407      
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=94f261fa142774418ea6a8d4ec11a957&units=metric&lang=pt_br`
-               console.log(url)
+
+
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const latitudeData = position.coords.latitude;
+            const longitudeData = position.coords.longitude;
+
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitudeData}&lon=${longitudeData}&appid=94f261fa142774418ea6a8d4ec11a957&units=metric&lang=pt_br`
+
             fetch(url)
-            .then( response => { return response.json() })
-            .then ( data => {
-                descricao.textContent = data.weather[0].description
-                latitude.textContent = "Latitude: " + data.coord.lat
-                longitude.textContent = "Longitude: " + data.coord.lon
-                vento.textContent = "Velocidade do vento: " + data.wind.speed + " km/h"
-                console.log(data)
+                .then(response => { return response.json() })
+                .then(data => {
 
-                switch(data.weather[0].description){
-                    case 'chuva leve':
-                    icon.src = 'animated/rainy-1.svg'
-                    break;
+                    setInterval(function () {
+                        var today = new Date().toLocaleTimeString();
+                        city.textContent = `${data.name} ${today}`
+                        temp.textContent = parseInt(data.main.temp) + " °C"
+                        descricao.textContent = data.weather[0].description
+                        latitude.textContent = "Latitude: " + data.coord.lat
+                        longitude.textContent = "Longitude: " + data.coord.lon
+                        vento.textContent = "Velocidade do vento: " + data.wind.speed + " km/h"
+                    });
 
-                    case 'algumas nuvens':
-                    icon.src = 'animated/cloudy-day-1.svg'
-                    break;
 
-                    case 'nublado':
-                    icon.src = 'animated/cloudy.svg'
-                    break;
 
-                    case 'chuva moderada':
-                    icon.src = 'animated/rainy-1.svg'
-                    break;
 
-                    case 'light rain':
-                    icon.src = 'animated/cloudy.svg'
-                    break;
 
-                    case 'clear sky':
-                    icon.src = 'animated/day.svg'
-                    break;
-                }
+                    switch (data.weather[0].description) {
+                        case 'chuva leve':
+                            icon.src = 'http://openweathermap.org/img/wn/10d.png'
+                            break;
 
-            })
-            .catch( error => {
-                console.log(error)
-            })
-        
-          
-    
+                        case 'algumas nuvens':
+                            icon.src = 'http://openweathermap.org/img/wn/03d.png'
+                            break;
+
+                        case 'nublado':
+                            icon.src = 'http://openweathermap.org/img/wn/04d.png'
+                            break;
+
+                        case 'chuva moderada':
+                            icon.src = 'http://openweathermap.org/img/wn/10d.png'
+                            break;
+
+                        case 'light rain':
+                            icon.src = 'http://openweathermap.org/img/wn/10d.png'
+                            break;
+
+                        case 'clear sky':
+                            icon.src = 'http://openweathermap.org/img/wn/01d.png'
+                            break;
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+        })
+
+    } else {
+        console.log("Geolocalização não é suportada neste navegador.");
+    }
+
 })
